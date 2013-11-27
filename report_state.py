b1fd335264edf8b627ac64e9f0fe900ae1906b43
Emmanuel Raviart <eraviart@easter-eggs.com>
2013-11-27 17:40:03 +0100

    Increase reported metrics.

--- report_state.py
+++ report_state.py
@@ -45,9 +45,9 @@ conf = None
 conv = custom_conv(baseconv, jsonconv, states)
 headers = None
 log = logging.getLogger(app_name)
+metrics = None
 request_headers = None
-states = None
-weight_by_dataset_name = None
+stats = None
 
 
 # Converters
@@ -131,25 +131,17 @@ def main():
     request_headers = headers.copy()
     request_headers['Content-Type'] = 'application/json'
 
-    request = urllib2.Request(urlparse.urljoin(conf['ckan_of_worms.site_url'], 'api/1/datasets/weights'),
+    request = urllib2.Request(urlparse.urljoin(conf['ckan_of_worms.site_url'], 'api/1/metrics'),
         headers = headers)
     response = urllib2.urlopen(request)
-    global weight_by_dataset_name
-    weight_by_dataset_name = conv.check(conv.pipe(
+    global metrics
+    metrics = conv.check(conv.pipe(
         cow_response_to_value,
         conv.test_isinstance(dict),
-        conv.uniform_mapping(
-            conv.pipe(
-                conv.test_isinstance(basestring),
-                conv.empty_to_none,
-                conv.not_none,
-                ),
-            conv.test_isinstance(float),
-            ),
         conv.not_none,
         ))(response.read(), state = conv.default_state)
 
-    send_states()
+    send_stats()
 
     fedmsg_conf = conv.check(conv.struct(
         dict(
@@ -193,52 +185,138 @@ def main():
             log.debug(u'Ignoring message: {}, {}'.format(topic, name))
             continue
         kind, action = topic[len(expected_topic_prefix):].split('.')
-        if kind == 'dataset':
+        if kind == 'account':
+            account = message['msg']
+            name = account['name']
+            if action == 'create':
+                metrics['accounts_count'] += 1
+                log.info(u'Updating stats after "{} account {}"'.format(action, name))
+                send_stats()
+            elif action == 'delete':
+                metrics['accounts_count'] -= 1
+                log.info(u'Updating stats after "{} account {}"'.format(action, name))
+                send_stats()
+            elif action != 'update':
+                log.warning(u'Unknown action "{}" for account "{}"'.format(action, account))
+        elif kind == 'dataset':
             dataset = message['msg']
             name = dataset['name']
-            existing_weight = weight_by_dataset_name.get(name)
+            existing_dataset_metrics = metrics['datasets'].get(name, {})
             if action in ('create', 'update'):
-                weight = dataset.get('weight')
+                dataset_metrics = dict(
+                    organization_name = (dataset.get('organization') or {}).get('name'),
+                    related_count = len(dataset.get('related') or []),
+                    resources = [
+                        dict(
+                            format = resource.get('format'),
+                            )
+                        for resource in (dataset.get('resources') or [])
+                        ],
+                    weight = dataset['weight'],
+                    )
             elif action == 'delete':
-                weight = None
+                dataset_metrics = None
             else:
                 log.warning(u'Unknown action "{}" for dataset "{}"'.format(action, dataset))
                 continue
-            if weight != existing_weight:
-                log.info(u'Updating states after "{} dataset {}"'.format(action, dataset['name']))
-                if weight is None:
-                    del weight_by_dataset_name[name]
+            if dataset_metrics != existing_dataset_metrics:
+                log.info(u'Updating stats after "{} dataset {}"'.format(action, name))
+                if dataset_metrics is None:
+                    del metrics['datasets'][name]
                 else:
-                    weight_by_dataset_name[name] = weight
-                send_states()
+                    metrics['datasets'][name] = dataset_metrics
+                send_stats()
+        elif kind == 'organization':
+            organization = message['msg']
+            name = organization['name']
+            existing_organization_metrics = metrics['organizations'].get(name, {})
+            if action in ('create', 'update'):
+                organization_metrics = dict(
+                    public_service = bool(organization.get('public_service')),
+                    )
+            elif action == 'delete':
+                organization_metrics = None
+            else:
+                log.warning(u'Unknown action "{}" for organization "{}"'.format(action, organization))
+                continue
+            if organization_metrics != existing_organization_metrics:
+                log.info(u'Updating stats after "{} organization {}"'.format(action, name))
+                if organization_metrics is None:
+                    del metrics['organizations'][name]
+                else:
+                    metrics['organizations'][name] = organization_metrics
+                send_stats()
         else:
             log.debug(u'TODO: Handle {}, {} for {}'.format(kind, action, message))
 
     return 0
 
 
-def send_states():
-    global states
-    states = dict(
+def median(values):
+    sorted_values = sorted(values)
+    values_count = len(sorted_values)
+    quotient, remainder = divmod(values_count, 2)
+    if remainder == 0:
+        return (sorted_values[quotient - 1] + sorted_values[quotient]) / 2.0
+    return sorted_values[quotient]
+
+
+def send_stats():
+    datasets_weight = [
+        weight
+        for weight in (
+            dataset['weight']
+            for dataset in metrics['datasets'].itervalues()
+            )
+        if weight is not None
+        ]
+    datasets_total_weight = sum(datasets_weight)
+    global stats
+    stats = dict(
         datasets = dict(
-            count = len(weight_by_dataset_name),
-            weights = sum(
-                weight
-                for weight in weight_by_dataset_name.itervalues()
-                if weight is not None
+            average_weight = datasets_total_weight / len(datasets_weight),
+            count = len(metrics['datasets']),
+            median_weight = median(datasets_weight),
+            total_weight = datasets_total_weight,
+            ),
+        formats = dict(
+            count = len(set(
+                resource['format']
+                for dataset in metrics['datasets'].itervalues()
+                for resource in dataset['resources']
+                )),
+            ),
+        organizations = dict(
+            count = len(metrics['organizations']),
+            public_services_count = sum(
+                1
+                for organization in metrics['organizations'].itervalues()
+                if organization['public_service']
                 ),
+            ),
+        related = dict(
+            count = sum(
+                dataset['related_count']
+                for dataset in metrics['datasets'].itervalues()
+                )
+            ),
+        resources = dict(
+            count = sum(
+                len(dataset['resources'])
+                for dataset in metrics['datasets'].itervalues()
+                )
             ),
         )
 
     request = urllib2.Request(urlparse.urljoin(conf['dactylo.site_url'], 'api/1/states'), headers = request_headers)
     request_data = dict(
         api_key = conf['dactylo.api_key'],
-        value = states,
+        value = stats,
         )
     try:
         response = urllib2.urlopen(request, json.dumps(request_data))
     except urllib2.HTTPError as response:
-        log.error(u'An error occured while setting states: {}'.format(states))
+        log.error(u'An error occured while setting stats: {}'.format(stats))
         response_text = response.read()
         try:
             response_dict = json.loads(response_text)
